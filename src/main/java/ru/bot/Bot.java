204e9c303a7fb622b32cbf7e306503b2c8b98ed4
OldMan35 <kostya_kv@mail.ru>
2022-06-12 18:09:50 +0300

    получение и обработка сообщения бота, добавил 2 кнопки в меню

--- src/main/java/ru/bot/Bot.java
+++ src/main/java/ru/bot/Bot.java
@@ -5,8 +5,15 @@ import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -22,25 +29,60 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    //метод для обновления сообщения из чата
+    //метод для обработки сообщения из чата и ответа на него
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
             switch (message.getText()) {
+                case "/start":
+                    sendTextMessage(message, "Ну что же, начнём?");
+                    break;
                 case "/help":
-                    sendMessage(message, "чем могу помочь?");
+                    sendTextMessage(message, "Чем могу помочь?");
                     break;
                 case "/settings":
-                    sendMessage(message, "что будем настраивать?");
+                    sendTextMessage(message, "Что будем настраивать?");
+                    break;
+                default:
+                    sendTextMessage(message, "Сформулируй свой вопрос иначе");
                     break;
             }
         }
     }
 
-    //отправить ответ на сообщение
-    private void sendMessage(Message message, String massageText) {
+    //метод для отправки ответа на сообщение
+    private void sendTextMessage(Message message, String massageText) {
         SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);//включение разметки
+        sendMessage.setChatId(message.getChatId().toString()); //определяем в какой конкретно чат мы должны отправить ответ
+        sendMessage.setReplyToMessageId(message.getMessageId()); //определяем на какое конкретно сообщение мы должны отправить ответ
+        sendMessage.setText(massageText);//текст ответа
+        //пытаемся отправить сообщение
+        try {
+            setButtons(sendMessage);
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void setButtons(SendMessage sendMessage) {
+        //создаем объект для клавиатуры
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        //установка параметров клавиатуры
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        //списко кнопок
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRow.add(new KeyboardButton("/help"));
+        keyboardRow.add(new KeyboardButton("/settings"));
+        keyboardRowList.add(keyboardRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
     }
 
     //получение имя бота
@@ -50,6 +92,6 @@ public class Bot extends TelegramLongPollingBot {
 
     //получения токена
     public String getBotToken() {
-        return "";
+        return "5307479270:AAHzyRFhACfFH_ek2SaaVwcjQiDfKmnBDxs";
     }
 }
